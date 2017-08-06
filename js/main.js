03e5f8f3a908ceea5f28da1c80f2007e9bdaf3c3
gcangela <gcangela90@gmail.com>
2017-08-06 21:04:25 +0200

    final version including back to top button

--- js/main.js
+++ js/main.js
@@ -13,3 +13,31 @@ function isNameInList(name) {
 }
 
 console.log('Is Pikachu a league of legends champion? ' , isNameInList('pikachu'));
+
+
+$(document).ready(function() {
+    var offset = 220;
+    var duration = 500;
+    $(window).scroll(function() {
+        if (jQuery(this).scrollTop() > offset) {
+            jQuery('.back-to-top').fadeIn(duration);
+        } else {
+            jQuery('.back-to-top').fadeOut(duration);
+        }
+    });
+
+    $('.back-to-top').click(function(event) {
+        event.preventDefault();
+        $('html, body').animate({scrollTop: 0}, duration);
+
+    })
+});
+
+
+
+
+
+
+
+
+
